ad964566feb12517e4de5be43c2ca38826fa7ca7
ciuszek <49906488+ciuszek@users.noreply.github.com>
2021-11-20 20:29:26 +0100

    added working Google sign in button, User model, AuthViewModel, TODO: Sign in by email, Sign up textbox and style of the Facebook button

--- app/src/main/java/com/example/noinstagram/FirstPage.kt
+++ app/src/main/java/com/example/noinstagram/FirstPage.kt
@@ -1,17 +1,24 @@
 package com.example.noinstagram
 
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Paint
 import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
 import androidx.compose.ui.graphics.nativeCanvas
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
@@ -26,10 +33,40 @@ import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.noinstagram.ui.theme.LightBlue
 import com.example.noinstagram.ui.theme.NoInstagramTheme
+import com.example.noinstagram.utils.AuthResultContract
+import com.example.noinstagram.viewmodel.AuthViewModel
 import com.facebook.login.widget.LoginButton
+import com.google.android.gms.common.api.ApiException
+import kotlinx.coroutines.launch
 
 @Composable
-fun FirstPage(navController: NavController) {
+fun FirstPage(navController: NavController, authViewModel: AuthViewModel) {
+    val coroutineScope = rememberCoroutineScope()
+    var text by remember { mutableStateOf<String?>(null) }
+    val user by remember(authViewModel) { authViewModel.user }.collectAsState()
+    val signInRequestCode = 1
+
+
+    val authResultLauncher =
+        rememberLauncherForActivityResult(contract = AuthResultContract()) { task ->
+            try {
+                val account = task?.getResult(ApiException::class.java)
+                if (account == null) {
+                    text = "Google sign in failed"
+                } else {
+                    coroutineScope.launch {
+                        authViewModel.signIn(
+                            email = account.email,
+                            displayName = account.displayName,
+                        )
+                    }
+                }
+            } catch (e: ApiException) {
+                text = "Google sign in failed"
+            }
+        }
+
+
     Surface(
         color = MaterialTheme.colors.primary,
         modifier = Modifier
@@ -38,12 +75,24 @@ fun FirstPage(navController: NavController) {
 
         FirstPageBackground()
 
-        FirstPageScreenContent(navController)
+        FirstPageScreenContent(
+            navController,
+            errorText = text,
+            onClick = {
+                text = null
+                authResultLauncher.launch(signInRequestCode)
+            })
     }
 }
 
 @Composable
-fun FirstPageScreenContent(navController: NavController) {
+fun FirstPageScreenContent(
+    navController: NavController,
+    errorText: String?,
+    onClick: () -> Unit
+) {
+    var isLoading by remember { mutableStateOf(false) }
+
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier.fillMaxWidth()
@@ -52,14 +101,86 @@ fun FirstPageScreenContent(navController: NavController) {
         AppTitleName()
         Spacer(Modifier.height(100.dp))
         FacebookButton()
+        Spacer(Modifier.height(30.dp))
+        GoogleButton(text = "Sign in with Google",
+            loadingText = "Signing in...",
+            isLoading = isLoading,
+            icon = painterResource(id = R.drawable.ic_google_logo),
+            onClick = {
+                isLoading = true
+                onClick()
+            })
+    }
+}
+
+@Composable
+fun GoogleButton(
+    text: String,
+    loadingText: String = "Signing in...",
+    icon: Painter,
+    isLoading: Boolean = false,
+    borderColor: Color = Color.LightGray,
+    backgroundColor: Color = MaterialTheme.colors.surface,
+    progressIndicatorColor: Color = MaterialTheme.colors.primary,
+    onClick: () -> Unit
+) {
+    Surface(
+        modifier = Modifier.clickable(
+            enabled = !isLoading,
+            onClick = onClick
+        ),
+        shape = RoundedCornerShape(22.dp),
+        border = BorderStroke(width = 1.dp, color = borderColor),
+        color = backgroundColor,
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(
+                    start = 12.dp,
+                    end = 16.dp,
+                    top = 12.dp,
+                    bottom = 12.dp
+                )
+                .animateContentSize(
+                    animationSpec = tween(
+                        durationMillis = 300,
+                        easing = LinearOutSlowInEasing
+                    )
+                ),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center,
+        ) {
+            Icon(
+                painter = icon,
+                contentDescription = "SignInButton",
+                tint = Color.Unspecified
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+
+            Text(text = if (isLoading) loadingText else text)
+            if (isLoading) {
+                Spacer(modifier = Modifier.width(16.dp))
+                CircularProgressIndicator(
+                    modifier = Modifier
+                        .height(16.dp)
+                        .width(16.dp),
+                    strokeWidth = 2.dp,
+                    color = progressIndicatorColor
+                )
+            }
+        }
     }
 }
 
 @Composable
 fun FacebookButton() {
-    AndroidView(factory = { context ->
-        LoginButton(context)
-    })
+    AndroidView(
+        factory = { context ->
+            LoginButton(context).apply {
+
+            }
+        },
+    )
 }
 
 @Composable
@@ -74,6 +195,7 @@ fun AppTitleName() {
         strokeWidth = strokePxValue
         strokeMiter = strokePxValue - 1
         strokeJoin = android.graphics.Paint.Join.ROUND
+        textAlign = android.graphics.Paint.Align.CENTER
     }
 
     val textPaint = Paint().asFrameworkPaint().apply {
@@ -81,8 +203,9 @@ fun AppTitleName() {
         style = android.graphics.Paint.Style.FILL
         textSize = pxValue
         color = android.graphics.Color.TRANSPARENT
-
+        textAlign = android.graphics.Paint.Align.CENTER
     }
+
 
     Canvas(
         modifier = Modifier.fillMaxWidth(),
@@ -90,25 +213,25 @@ fun AppTitleName() {
             drawIntoCanvas {
                 it.nativeCanvas.drawText(
                     "No",
-                    130.dp.toPx(),
+                    center.x,
                     0.dp.toPx(),
                     textPaintStroke
                 )
                 it.nativeCanvas.drawText(
                     "No",
-                    130.dp.toPx(),
+                    center.x,
                     0.dp.toPx(),
                     textPaint
                 )
                 it.nativeCanvas.drawText(
                     "Instagram",
-                    50.dp.toPx(),
+                    center.x,
                     50.dp.toPx(),
                     textPaintStroke
                 )
                 it.nativeCanvas.drawText(
                     "Instagram",
-                    50.dp.toPx(),
+                    center.x,
                     50.dp.toPx(),
                     textPaint
                 )
@@ -127,6 +250,7 @@ fun AppTitleName() {
 
 }
 
+
 @Composable
 fun FirstPageBackground() {
     Image(
@@ -141,7 +265,7 @@ fun FirstPageBackground() {
 @Composable
 private fun FirstPagePreview() {
     NoInstagramTheme {
-        FirstPage(navController = rememberNavController())
+        FirstPage(navController = rememberNavController(), authViewModel = AuthViewModel())
     }
 }
 
--- app/src/main/java/com/example/noinstagram/MainActivity.kt
+++ app/src/main/java/com/example/noinstagram/MainActivity.kt
@@ -3,16 +3,19 @@ package com.example.noinstagram
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.noinstagram.ui.theme.NoInstagramTheme
+import com.example.noinstagram.viewmodel.AuthViewModel
 
 
 class MainActivity : ComponentActivity() {
+    private val authViewModel: AuthViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -21,7 +24,7 @@ class MainActivity : ComponentActivity() {
 
                 NavHost(navController, "FirstPage") {
                     composable("FirstPage") {
-                        FirstPage(navController)
+                        FirstPage(navController, authViewModel)
                     }
                 }
             }
@@ -32,18 +35,4 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String) {
     Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    NoInstagramTheme {
-        val navController = rememberNavController()
-
-        NavHost(navController, "FirstPage") {
-            composable("FirstPage") {
-                FirstPage(navController)
-            }
-        }
-    }
 }
