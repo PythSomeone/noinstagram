bce7292ce17732522b87281184d9e348a5263aeb
PythSomeone <40042814+PythSomeone@users.noreply.github.com>
2021-11-30 23:26:52 +0100

    added creating user with email and password, excluding username

---
+++ app/src/main/java/com/example/noinstagram/viewmodel/RegisterViewModel.kt
@@ -0,0 +1,27 @@
+package com.example.noinstagram.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.noinstagram.utils.LoadingState
+import com.google.firebase.auth.AuthCredential
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+
+class RegisterViewModel : ViewModel() {
+    val loadingState = MutableStateFlow(LoadingState.IDLE)
+
+    fun createUserWithEmailAndPassword(email: String, password: String) = viewModelScope.launch {
+        try {
+            loadingState.emit(LoadingState.LOADING)
+            Firebase.auth.createUserWithEmailAndPassword(email, password).await()
+            loadingState.emit(LoadingState.LOADED)
+        } catch (e: Exception) {
+            loadingState.emit(LoadingState.error(e.localizedMessage))
+        }
+    }
+
+}
