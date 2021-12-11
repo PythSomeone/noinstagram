8a787091e17b89085fd8c048d15219cba68ab932
kamilmaciuszek <kamil.maciuszek@gmail.com>
2021-12-11 10:44:44 +0100

    Added Zoom, rotation and pan to the homescreen images.

--- app/src/main/java/com/example/noinstagram/ui/components/HomeScreenCard.kt
+++ app/src/main/java/com/example/noinstagram/ui/components/HomeScreenCard.kt
@@ -4,12 +4,11 @@ import android.text.format.DateUtils
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.*
+import androidx.compose.foundation.gestures.detectTransformGestures
 import androidx.compose.foundation.indication
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Divider
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
@@ -22,25 +21,18 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.*
-import androidx.compose.ui.input.pointer.consumePositionChange
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.input.pointer.positionChange
-import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.unit.toSize
 import coil.compose.rememberImagePainter
 import com.example.noinstagram.model.Post
 import com.example.noinstagram.model.UserModel
 import com.example.noinstagram.ui.buttons.AnimLikeButton
 import kotlin.math.PI
 import kotlin.math.cos
-import kotlin.math.roundToInt
 import kotlin.math.sin
 
 
@@ -72,11 +64,12 @@ fun PostView(
                             onGesture = { centroid, pan, gestureZoom, gestureRotate ->
                                 val oldScale = zoom
                                 val newScale = zoom * gestureZoom
-                                if (oldScale != newScale){
-                                offset = (offset + centroid / oldScale).rotateBy(gestureRotate) -
-                                        (centroid / newScale + pan / oldScale)
-                                zoom = newScale
-                                angle += gestureRotate
+                                if (oldScale != newScale) {
+                                    offset =
+                                        (offset + centroid / oldScale).rotateBy(gestureRotate) -
+                                                (centroid / newScale + pan / oldScale)
+                                    zoom = newScale
+                                    angle += gestureRotate
                                 }
                             }
                         )
