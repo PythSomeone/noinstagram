58e9c89401407ac48a7b87c3d4e23113ec65ca17
Huszan <mateuszjacenty1@gmail.com>
2021-12-17 10:36:46 +0100

    Finished storage handler
    We can now upload images for posts and users
    I also tweaked our models a little to prevent a few nulls. I will fix it soon

--- app/src/main/java/com/example/noinstagram/model/UserModel.kt
+++ app/src/main/java/com/example/noinstagram/model/UserModel.kt
@@ -1,10 +1,15 @@
 package com.example.noinstagram.model
 
+import android.net.Uri
+import com.google.android.gms.tasks.Task
+
 data class UserModel(
     var id: String? = null,
     val email: String? = "",
     val displayName: String? = "",
     val description: String? = "",
+    var photo: String? = "",
+    var postLikes: MutableList<String?> = mutableListOf(),
     var following: MutableList<String?> = mutableListOf(),
     var followers: MutableList<String?> = mutableListOf()
 )
