865465ca42f908f8b181912055278ef68f9a4423
PythSomeone <40042814+PythSomeone@users.noreply.github.com>
2022-01-06 16:50:03 +0100

    redirect after login

--- app/src/main/java/com/example/noinstagram/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/example/noinstagram/viewmodel/LoginViewModel.kt
@@ -2,6 +2,7 @@ package com.example.noinstagram.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.navigation.NavController
 import com.example.noinstagram.utils.LoadingState
 import com.google.firebase.auth.AuthCredential
 import com.google.firebase.auth.FirebaseAuth
@@ -14,11 +15,12 @@ import kotlinx.coroutines.tasks.await
 class LoginViewModel : ViewModel() {
     val loadingState = MutableStateFlow(LoadingState.IDLE)
 
-    fun signInWithEmailAndPassword(email: String, password: String) = viewModelScope.launch {
+    fun signInWithEmailAndPassword(email: String, password: String, navController : NavController) = viewModelScope.launch {
         try {
             loadingState.emit(LoadingState.LOADING)
             Firebase.auth.signInWithEmailAndPassword(email, password).await()
             loadingState.emit(LoadingState.LOADED)
+            navController.navigate("HomePage")
         } catch (e: Exception) {
             loadingState.emit(LoadingState.error(e.localizedMessage))
         }
