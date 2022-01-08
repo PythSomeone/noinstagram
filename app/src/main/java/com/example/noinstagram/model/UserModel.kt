e351f48d139a4512d76ad0117bb655827bd15a2c
Huszan <mateuszjacenty1@gmail.com>
2022-01-08 13:55:11 +0100

    Update UserModel.kt
    New default image

--- app/src/main/java/com/example/noinstagram/model/UserModel.kt
+++ app/src/main/java/com/example/noinstagram/model/UserModel.kt
@@ -5,7 +5,7 @@ data class UserModel(
     val email: String? = "",
     var displayName: String? = "",
     var description: String? = "",
-    var image: String? = "https://firebasestorage.googleapis.com/v0/b/noinstagram-e6c32.appspot.com/o/DEFAULT.png?alt=media&token=5909137c-8e0c-48d1-aa4d-6061cb0b6132",
+    var image: String? = "https://firebasestorage.googleapis.com/v0/b/noinstagram-e6c32.appspot.com/o/DEFAULT.png?alt=media&token=3e9659e8-3f5c-4bdc-a338-7b321ec0a441",
     var postLikes: MutableList<String?> = mutableListOf(),
     var following: MutableList<String?> = mutableListOf(),
     var followers: MutableList<String?> = mutableListOf()
